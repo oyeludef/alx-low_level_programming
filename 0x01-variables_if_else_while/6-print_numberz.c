d1604ab71a41c951b58b7f5088e68655575f0634
Oyeludef <oyeludeolufemi@gmail.com>
2023-03-17 21:38:40 +0000

    zero to nine with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: always return 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{	putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
