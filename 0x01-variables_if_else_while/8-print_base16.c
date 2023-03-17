e2bcf94aebc5efaa72281a91ade17a29a06e31f6
Oyeludef <oyeludeolufemi@gmail.com>
2023-03-17 22:16:26 +0000

    base 16 characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: always return 0
+  *
+  */
+int main(void)
+{
+	int n;
+	int a;
+
+	for (n = 48; n <= 57; n++)
+	{	putchar(n);
+	}
+	for (a = 97; a <= 102; a++)
+	{	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
