e5cdd4960c8b0679bc149907adc7bd2c6fda2c4c
Oyeludef <oyeludeolufemi@gmail.com>
2023-03-21 19:30:04 +0000

    task one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * print_alphabet - Prints lowercase alphabets
+  *
+  * Return: always return 0
+  *
+  */
+void print_alphabet(void)
+{	int a;
+	for (a=97; a<=122; a++)
+	{_putchar(a);
+	}
+	_putchar('\n';)
+	return (0)
+}
