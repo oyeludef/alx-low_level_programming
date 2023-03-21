37955b975c090ec53503ea35dee4295394664cf0
Oyeludef <oyeludeolufemi@gmail.com>
2023-03-21 23:12:30 +0000

    the sixth task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * _abs - calculates absolute values of numbers
+  *
+  * @n: an integer
+  *
+  * Return: absolute values
+  *
+  */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
