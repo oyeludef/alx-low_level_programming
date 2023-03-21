81a7597d3e9e8a62b4a1c4b104501b94482f8b17
Oyeludef <oyeludeolufemi@gmail.com>
2023-03-21 20:34:18 +0000

    task two

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - print lowercase alhabets 10 times
+  *
+  * Return: Always0
+  *
+  */
+void print_alphabet_x10(void)
+{	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		putchar('\n');
+	}
+}
