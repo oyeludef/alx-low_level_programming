9fe651ff648cd397ce1c20f664865b1bd33eaf5b
Oyeludef <oyeludeolufemi@gmail.com>
2023-03-21 23:55:25 +0000

    7th task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_last_digit - prints last digit of a number
+  *
+  * @n: integer supplied
+  *
+  * Return: last digits
+  *
+  */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l > 0)
+	{
+		_putchar(l + 48);
+		return (l);
+
+	}
+	else
+		_putchar(-l + 48);
+		return (-l);
+}
