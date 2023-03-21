6fc4c0db6eebd1883b256055dcc02c8ad75a253f
Oyeludef <oyeludeolufemi@gmail.com>
2023-03-21 22:18:17 +0000

    the fourth task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-  * _isalpha - prints uppercase characters.
+  * _isalpha - check aphabetic characters.
   *
   * @c: is a character in ASCII code
   *
-  * Return: 1 for uppercase and 0 when otherwise
+  * Return: 1 for alphabets and 0 when otherwise
   *
   */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
