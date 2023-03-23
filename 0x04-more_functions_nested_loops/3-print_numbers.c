1c7b4be41016fc323b4a3f62e1b72eca38c33848
Oyeludef <oyeludeolufemi@gmail.com>
2023-03-23 11:22:12 +0000

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * print_numbers - print numbers 0 to 9
+  *
+  */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar('a');
+	}
+	putchar('\n');
+}
