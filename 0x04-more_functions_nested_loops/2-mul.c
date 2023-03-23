84e1113fde29ce7fe81a1667b002abf744c59440
Oyeludef <oyeludeolufemi@gmail.com>
2023-03-23 11:01:25 +0000

    multiply integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * mul -multiply two integers
+  *
+  * @a: is first number
+  * @b: is second number
+  *
+  * Return: return ans
+  *
+  */
+int mul(int a, int b)
+{
+	int ans;
+
+	ans = a * b;
+	return (ans);
+}
