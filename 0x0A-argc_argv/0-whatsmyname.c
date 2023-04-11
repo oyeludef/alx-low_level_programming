0ec530b7f218fcf1803882f40e2c235468c18174
Oyeludef <oyeludeolufemi@gmail.com>
2023-04-11 00:01:12 +0000

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - program that prints its name
+  *
+  * @argv: array of listed comman
+  *
+  * @argc: Argc argument
+  *
+  * Return: always 0
+  */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+}
