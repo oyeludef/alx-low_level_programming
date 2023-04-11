f437174939525b5a2c124458f346c7548d1b1433
Oyeludef <oyeludeolufemi@gmail.com>
2023-04-11 01:10:57 +0000

    prints the minimum number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,7 +13,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, ncoins = 0;
+	int cents, numcoins = 0;
 
 	if (argc == 1 || argc > 2)
 	{
@@ -35,8 +35,8 @@ int main(int argc, char *argv[])
 			cents -= 2;
 		else if (cents >= 1)
 			cents -= 1;
-		ncoins += 1;
+		numcoins += 1;
 	}
-	printf("%d\n", ncoins);
+	printf("%d\n", numcoins);
 	return (0);
 }
