faffb632f9b1f2c443ba03c0ab4395829b47b40c
Oyeludef <oyeludeolufemi@gmail.com>
2023-04-14 23:33:31 +0000

    creating standard library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * _isupper - check for uppercase alphabeth
+  *
+  * @c: alphabet
+  *
+  * Return: 1 for uppercase 0 if otherwise
+  *
+  */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
