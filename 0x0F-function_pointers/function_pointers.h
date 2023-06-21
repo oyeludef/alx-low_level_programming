d7eb7bdd9cedf794bac5181fcdc1639ad97c8794
Oyeludef <oyeludeolufemi@gmail.com>
2023-06-21 18:21:20 +0000

    updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,6 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-int op_add(int a, int b);
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int op_sub(int a, int b);
-int _putchar(char c);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 #endif
